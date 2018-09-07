ce0470f22e9712cc59e6a46738b07608ef49ee49
Paula H <42469219+PaulaHolubek@users.noreply.github.com>
2018-09-07 13:21:44 -0400

    Add files via upload

--- JS/slots.js
+++ JS/slots.js
@@ -1,153 +1,72 @@
-
-//Hiding the Goodbye paragraph
-    $('#see').hide();
-
 //Code will run when document is ready and spin button has been clicked
 $(document).ready(function () {
     $("#spin").click(function () {
-        var result;
-        var result1;
-        var result2;
-        var box1;
-        var box2;
-        var box3;
-        var msgwin = "Congratulations, you won!";
-        var msgnowin = "Sorry, try again";
-        var i;
-        
-		//determines random number for the first box and adds the corresponding value to the class
-        for (i = 1; i <= 10; i++) {
-            result = Math.floor(Math.random() * 10);
+        spin("tb1");
+        spin("tb2");
+        spin("tb3");
+        checkMatchingBoxes();
+    })
 
-            if (result == 0 || result > 3) {
+    function spin(tbId) {
+        var counter;
+
+        //determines random number for the tb1 box and adds the corresponding value to the class
+        for (counter = 0; counter < 10; counter++) {
+            var rndNum = Math.floor(Math.random() * 10);
+
+            if (rndNum == 0 || rndNum > 3)
                 continue;
-            }
-            else if (result == 1) {
-                //document.write('Assign Class a Value of one' + '<br>');
-                //$("#first").removeClass("one two three");
-                //$("#first").addClass("one");
-				$("#first").attr('class', 'one');
-                break;
+
+            var el = document.getElementById(tbId);
+
+            if (rndNum == 1) {
+                el.className = "one";
             }
 
-            else if (result == 2) {
-                //document.write('Assign Class a Value of two' + '<br>');
-                //$("#first").removeClass("one two three");
-                //$("#first").addClass("two");
-				$("#first").attr('class', 'two');
-                break;
-            }
-            else if (result == 3) {
-                //document.write('Assign Class a Value of three' + '<br>');
-               // $("#first").removeClass("one two three");
-               // $("#first").addClass("three");
-				$("#first").attr('class', 'three');
-                break;
-            }
-            else {
-                ;
+            if (rndNum == 2) {
+                el.className = "two";
             }
 
-        };
+            if (rndNum == 3) {
+                el.className = "three";
+            }
 
-		//determines random number for the second box and adds the corresponding value to the class
-        for (i = 1; i <= 10; i++) {
-            result1 = Math.floor(Math.random() * 10);
+        }
+    }
 
-            if (result1 == 0 || result1 > 3) {
-                continue;
-            }
-            else if (result1 == 1) {
-                //document.write('Assign Class a Value of one' + '<br>');
-                //$("#second").removeClass("one two three");
-                //$("#second").addClass("one");
-				$("#second").attr('class', 'one');
-                break;
-            }
-            else if (result1 == 2) {
-                //document.write('Assign Class a Value of two' + '<br>');
-                //$("#second").removeClass("one two three");
-               // $("#second").addClass("two");
-				$("#second").attr('class', 'two');
-                break;
-            }
-            else if (result1 == 3) {
-                //document.write('Assign Class a Value of three' + '<br>');
-                //$("#second").removeClass("one two three");
-                //$("#second").addClass("three");
-				$("#second").attr('class', 'three');
-                break;
-            }
-            else {
-                ;
-            }
-        };
+    function checkMatchingBoxes() {
+        var msgWin = "Congratulations, you won!";
+        var msgLost = "Sorry, try again";
 
-		//determines random number for the third box and adds the corresponding value to the class
-        for (i = 1; i <= 10; i++) {
-            result2 = Math.floor(Math.random() * 10);
+        //gets the class name for the tb1, second and third boxes and stores the values in separate variables
+        var firstbx = document.getElementById("tb1").getAttribute("class");
+        var secondbx = document.getElementById("tb2").getAttribute("class");
+        var thirdbx = document.getElementById("tb3").getAttribute("class");
 
-            if (result2 == 0 || result2 > 3) {
-                continue;
-            }
-            else if (result2 == 1) {
-                //document.write('Assign Class a Value of one' + '<br>');
-                //$("#third").removeClass("one two three");
-                //$("#third").addClass("one");
-				$("#third").attr('class', 'one');
-                break;
-            }
-            else if (result2 == 2) {
-                //document.write('Assign Class a Value of two' + '<br>');
-                //$("#third").removeClass("one two three");
-                //$("#third").addClass("two");
-				$("#third").attr('class', 'two');
-                break;
-            }
-            else if (result2 == 3) {
-                //document.write('Assign Class a Value of three' + '<br>');
-                //$("#third").removeClass("one two three");
-                //$("#third").addClass("three");
-				$("#third").attr('class', 'three');
-                break;
-            }
-            else {
-                ;
-            }
-        };
-		
-		//gets the class name for the first, second and third boxes and stores the values in separate variables
-        var box1 = document.getElementById("first").getAttribute("class");
-        var box2 = document.getElementById("second").getAttribute("class");
-        var box3 = document.getElementById("third").getAttribute("class");
-		
-		//variables are checked to see if all 3 are the same. 
-		//If so, a congrats message appears on the screen
-		//If not, a try again message appears on the screen
-        if (box1 == box2 && box1 == box3) {
-            document.getElementById('msg').innerHTML = msgwin;
+        //variables are checked to see if all 3 are the same. 
+        //If so, a congrats message appears on the screen
+        //If not, a try again message appears on the screen
+        if (firstbx == secondbx && firstbx == thirdbx) {
+            document.getElementById('msg').innerHTML = msgWin;
         }
         else {
-            document.getElementById('msg').innerHTML = msgnowin;
+            document.getElementById('msg').innerHTML = msgLost;
         }
+    }
+
+    //When the quit button is clicked, the screen fades away
+    //and Goodbye fades in on the screen
+    $("#quit").click(function () {
+        $("div").fadeOut(3000);
+        $("#sayGoodBye").fadeIn(2000);
     });
 
-});
-		//When the quit button is clicked, the screen fades away
-		//and Goodbye fades in on the screen
-        $("#quit").click(function () {
-            $("div").fadeOut(3000);
-        $("p#see").fadeIn(2000);
+    //Button text changes when mouse is on top of it
+    $("#spin").mouseover(function () {
+        $("#spin").text("SPIN NOW")
     });
-	
-		//Button text changes when mouse is on top of it
-        $("#spin").mouseover(function () {
-            $("#spin").text("SPIN NOW")
-            });
-		//Button text changes when mouse is not on top of it
-        $("#spin").mouseout(function () {
-            $("#spin").text("Spin Again")
-        });
-
-
-
+    //Button text changes when mouse is not on top of it
+    $("#spin").mouseout(function () {
+        $("#spin").text("Spin Again")
+    });
+})
